2ce815b9c3531087284fff592764f9a10a34251b
keithmalkin <keithmalkin@fastmail.net>
2022-10-27 08:56:22 -0400

    Xcode 3.1.4 template

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  PowerBattle
+//
+//  Created by Keith Malkin on 10/27/22.
+//  Copyright __MyCompanyName__ 2022 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PowerBattle
+//
+//  Created by Keith Malkin on 10/27/22.
+//  Copyright __MyCompanyName__ 2022 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
